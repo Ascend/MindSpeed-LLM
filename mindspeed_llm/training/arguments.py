3976fc48d359338f9e4606ffd93e99940c6b261f
丁子叉 <dingzilin@huawei.com>
2025-04-10 07:19:13 +0000

    !2521 [bugfix]fix dummy args Merge pull request !2521 from 丁子叉/2.0.0

--- mindspeed_llm/training/arguments.py
+++ mindspeed_llm/training/arguments.py
@@ -1377,7 +1377,6 @@ def _add_dummy_args(args):
     args.megatron_cp_in_bnsd = False
     args.use_fusion_attn_v2 = False
     args.npu_deterministic = False
-    args.schedules_method = None
 
 
 def _validate_noop_layer(args):
