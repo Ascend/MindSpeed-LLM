5ab94b72bfce352c43a9fee237408959dc947a52
qu_yueze <quyueze@h-partners.com>
2025-05-09 08:38:39 +0000

    !2648 fix and add sh of qwen3 Merge pull request !2648 from qu_yueze/master

--- tests/0day/qwen3/qwen3-30b-a3b/ckpt_convert_qwen3_a3b_mcore2hf.sh
+++ tests/0day/qwen3/qwen3-30b-a3b/ckpt_convert_qwen3_a3b_mcore2hf.sh
@@ -1,19 +1,17 @@
 # 修改 ascend-toolkit 路径
-export CUDA_DEVICE_MAX_CONNECTIONS=1
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 
 python convert_ckpt.py \
     --use-mcore-models \
     --model-type GPT \
-    --load-model-type hf \
-    --save-model-type mg \
+    --load-model-type mg \
+    --save-model-type hf \
     --target-tensor-parallel-size 1 \
-    --target-pipeline-parallel-size 8 \
+    --target-pipeline-parallel-size 1 \
     --target-expert-parallel-size 1 \
     --spec mindspeed_llm.tasks.models.spec.qwen3_spec layer_spec \
-    --load-dir ./model_from_hf/qwen3_moe_hf/ \
-    --save-dir ./model_weights/qwen3_moe_mcore/ \
-    --tokenizer-model ./model_from_hf/qwen3_moe_hf/tokenizer.json \
+    --save-dir ./model_from_hf/qwen3_moe_hf/ \
+    --load-dir ./model_weights/qwen3_moe_mcore/ \
     --model-type-hf qwen3-moe \
     --moe-grouped-gemm \
     --params-dtype bf16
