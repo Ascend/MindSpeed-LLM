ed47019e696363b201654c56a5e1354ef995cf7e
shengjy <shengjiayi@huawei.com>
2025-06-13 07:26:37 +0000

    !2814 [pytorch][bugfix]dualpipe mtp micro batch loss scale Merge pull request !2814 from shengjy/dpscale

--- mindspeed_llm/core/pipeline_parallel/dualpipe/adaptor.py
+++ mindspeed_llm/core/pipeline_parallel/dualpipe/adaptor.py
@@ -1,5 +1,7 @@
 #  Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+from functools import wraps
 
+import torch
 from megatron.training.utils import print_rank_0
 
 try:
@@ -15,7 +17,7 @@ try:
 except ImportError:
     pass
 
-from mindspeed_llm.core.transformer.multi_token_prediction import MultiTokenPredictionLayer
+from mindspeed_llm.core.transformer.multi_token_prediction import MultiTokenPredictionLayer, MTPLossAutoScaler
 
 
 def dualpipe_register_patches(MegatronAdaptation):
@@ -38,6 +40,12 @@ def dualpipe_register_patches(MegatronAdaptation):
         from mindspeed.core.pipeline_parallel.dualpipev.dualpipev_chunks import (
             get_model, dualpipev_fp16forward, get_num_layers_to_build, train_step, _allreduce_embedding_grads_wrapper
         )
+        MegatronAdaptation.register(
+            "mindspeed.core.pipeline_parallel.dualpipev.dualpipev_schedules.forward_step_with_model_graph",
+            dualpipe_forward_step_wrapper)
+        MegatronAdaptation.register(
+            "mindspeed.core.pipeline_parallel.dualpipev.dualpipev_schedules.forward_step_no_model_graph",
+            dualpipe_forward_step_wrapper)
         MegatronAdaptation.register('megatron.training.training.get_model', get_model)
         MegatronAdaptation.register('megatron.training.training.train_step', train_step)
         MegatronAdaptation.register('megatron.core.pipeline_parallel.schedules.forward_backward_pipelining_without_interleaving',
@@ -52,3 +60,46 @@ def dualpipe_register_patches(MegatronAdaptation):
                                     forward_backward_pipelining_with_interleaving)
 
 
+def dualpipe_forward_step_wrapper(fn):
+    @wraps(fn)
+    def wrapper(
+            forward_step_func,
+            model_chunk_id,
+            data_iterator,
+            model,
+            num_microbatches,
+            input_tensor,
+            forward_data_store,
+            config,
+            *args,
+            **kwargs,
+    ):
+        output, num_tokens = fn(
+            forward_step_func,
+            model_chunk_id,
+            data_iterator,
+            model,
+            num_microbatches,
+            input_tensor,
+            forward_data_store,
+            config,
+            *args,
+            **kwargs,
+        )
+
+        # Set the loss scale for Multi-Token Prediction (MTP) loss.
+        if hasattr(config, 'mtp_num_layers') and config.mtp_num_layers is not None:
+            # Calculate the loss scale based on the grad_scale_func if available, else default to 1.
+            loss_scale = (
+                config.grad_scale_func(torch.tensor(1.0))
+                if config.grad_scale_func is not None
+                else torch.tensor(1.0)
+            )
+            # Set the loss scale
+            if config.calculate_per_token_loss:
+                MTPLossAutoScaler.set_loss_scale(loss_scale)
+            else:
+                MTPLossAutoScaler.set_loss_scale(loss_scale / num_microbatches)
+        return output, num_tokens
+
+    return wrapper
