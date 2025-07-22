bd74154d181772b9148cb6ebaabc70c6b5ea8291
qu_yueze <quyueze@h-partners.com>
2025-07-22 03:38:07 +0000

    !3037 [pytorch][bugfix]fix ckpt of mamba2 Merge pull request !3037 from qu_yueze/master

--- examples/mcore/mamba2/ckpt_convert_mamba2_2.7b_hf2mg.sh
+++ examples/mcore/mamba2/ckpt_convert_mamba2_2.7b_hf2mg.sh
@@ -3,11 +3,11 @@ source /usr/local/Ascend/ascend-toolkit/set_env.sh
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 python mindspeed_llm/tasks/checkpoint/convert_ckpt_mamba2.py \
-    --load-model-type mg \
-    --save-model-type hf \
-    --load-dir ./ckpt/mamba2-tp4pp2 \
-    --save-dir ./ckpt/mamba2-tp2pp4 \
-    --target-pp-size 8 \
+    --load-model-type hf \
+    --save-model-type mg \
+    --load-dir ./ckpt/mamba2-hf \
+    --save-dir ./ckpt/mamba2-tp1pp4 \
+    --target-pp-size 4 \
     --target-tp-size 1 \
     --d-model 2560 \
     --mamba-d-state 128 \
