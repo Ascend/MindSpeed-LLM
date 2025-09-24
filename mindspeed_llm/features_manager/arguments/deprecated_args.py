39dce2a5d179a6d80ce998004a430c4358d72dea
yanzhixiao <yanzhixiao@h-partners.com>
2025-09-24 01:39:29 +0000

    !3364 [pytorch][bugfix] bugfix for master Merge pull request !3364 from yanzhixiao/bugfix-0923

--- mindspeed_llm/features_manager/arguments/deprecated_args.py
+++ mindspeed_llm/features_manager/arguments/deprecated_args.py
@@ -44,14 +44,15 @@ class DeprecatedArgsFeature(MindSpeedFeature):
         group.add_argument('--multi-head-latent-attention', action='store_true', default=False, dest='deprecated_multi_head_latent_attention',
                            help='Use Multi-head Latent Attention(MLA)'
                                 'Note: this option is deprecated, please use --multi-latent-attention instead!')
-        group.add_argument('--rope-scaling-beta-fast', type=int, default=32, dest='deprecated_rope_scaling_beta_fast',
+        group.add_argument('--rope-scaling-beta-fast', type=int, default=None, dest='deprecated_rope_scaling_beta_fast',
                            help='Yarn rope: rope beta fast'
                                 'Note: this option is deprecated, please use --beta-fast instead!')
-        group.add_argument('--rope-scaling-beta-slow', type=int, default=1, dest='deprecated_rope_scaling_beta_slow',
+        group.add_argument('--rope-scaling-beta-slow', type=int, default=None, dest='deprecated_rope_scaling_beta_slow',
                            help='Yarn rope: rope beta slow'
                                 'Note: this option is deprecated, please use --beta-slow instead!')
 
     def validate_args(self, args):
+        # If deprecated argument are used instead of new argument, we assign the deprecated argument to the new argument and issue a warning
         if args.deprecated_use_deter_comp and not args.npu_deterministic:
             warnings.warn(
                 """The '--use-deter-comp' argument is deprecated and will be removed in the next future version, 
@@ -67,7 +68,7 @@ class DeprecatedArgsFeature(MindSpeedFeature):
                 """The '--cp-attention-mask-type' argument is deprecated and will be removed in the next future version, 
                    please use '--attention-mask-type' instead!""", DeprecationWarning)
             args.attention_mask_type = args.deprecated_cp_attention_mask_type
-        if args.deprecated_moe_intermediate_size and not args.moe_ffn_hidden_size:
+        if args.deprecated_moe_intermediate_size:
             warnings.warn(
                 """The '--moe-intermediate-size' argument is deprecated and will be removed in the next future version, 
                    please use '--moe-ffn-hidden-size' instead!""", DeprecationWarning)
@@ -87,12 +88,12 @@ class DeprecatedArgsFeature(MindSpeedFeature):
                 """The '--routed-scaling-factor' argument is deprecated and will be removed in the next future version, 
                    please use '--moe-router-topk-scaling-factor' instead!""", DeprecationWarning)
             args.moe_router_topk_scaling_factor = args.deprecated_topk_group
-        if args.deprecated_qk_rope_head_dim and args.qk_pos_emb_head_dim:
+        if args.deprecated_qk_rope_head_dim:
             warnings.warn(
                 """The '--qk-rope-head-dim' argument is deprecated and will be removed in the next future version, 
                    please use '--qk-pos-emb-head-dim' instead!""", DeprecationWarning)
             args.qk_pos_emb_head_dim = args.deprecated_qk_rope_head_dim
-        if args.deprecated_qk_nope_head_dim and args.qk_head_dim:
+        if args.deprecated_qk_nope_head_dim:
             warnings.warn(
                 """The '--qk-nope-head-dim' argument is deprecated and will be removed in the next future version, 
                    please use '--qk-head-dim' instead!""", DeprecationWarning)
@@ -102,12 +103,12 @@ class DeprecatedArgsFeature(MindSpeedFeature):
                 """The '--multi-head-latent-attention' argument is deprecated and will be removed in the next future version, 
                    please use '--multi-latent-attention' instead!""", DeprecationWarning)
             args.multi_latent_attention = args.deprecated_multi_head_latent_attention
-        if args.deprecated_rope_scaling_beta_fast != 32 and args.beta_fast == 32:
+        if args.deprecated_rope_scaling_beta_fast:
             warnings.warn(
                 """The '--rope-scaling-beta-fast' argument is deprecated and will be removed in the next future version, 
                    please use 'beta-fast' instead!""", DeprecationWarning)
             args.beta_fast = args.deprecated_rope_scaling_beta_fast
-        if args.deprecated_rope_scaling_beta_slow != 1 and args.beta_slow == 1:
+        if args.deprecated_rope_scaling_beta_slow:
             warnings.warn(
                 """The '--rope-scaling-beta-slow' argument is deprecated and will be removed in the next future version, 
                    please use 'beta-slow' instead!""", DeprecationWarning)
