619c66b1d43cceb64bda0788d1711d5723b20801
EX_mitsuX <yangjie409@h-partners.com>
2025-10-13 15:45:12 +0800

    v3flora update！！
    Co-authored-by: EX_mitsu<yangjie409@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3474 merge master into master
    
    v3flora update！！
    
    Created-by: EX_mitsuX
    Commit-by: EX_mitsu
    Merged-by: ascend-robot
    Description: 1.修改脚本 提高该场景下训练效果。
    
    See merge request: Ascend/MindSpeed-LLM!3474

--- examples/mcore/deepseek3/tune_deepseek3_671b_4k_lora_ptd.sh
+++ examples/mcore/deepseek3/tune_deepseek3_671b_4k_lora_ptd.sh
@@ -2,6 +2,8 @@
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
 export HCCL_CONNECT_TIMEOUT=3600
+export STREAMS_PER_DEVICE=32
+export CPU_AFFINITY_CONF=1,lazy_bind:0
 
 NPUS_PER_NODE=8
 MASTER_ADDR=localhost #主节点IP
@@ -46,6 +48,7 @@ MLA_ARGS="
 MOE_ARGS="
     --moe-permutation-async-comm \
     --moe-token-dispatcher-type alltoall_seq \
+    --moe-permute-fusion \
     --first-k-dense-replace 3 \
     --moe-layer-freq 1 \
     --n-shared-experts 1 \
