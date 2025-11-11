cd0539f84d25ec82fb317f026b7f420f323060e6
JZY_SC30 <jiangzhiyou@huawei.com>
2025-11-11 14:17:24 +0800

    [pytorch][feature]update ckpt_convert script to v2 (Qwen3&Qwen3_moe)
    Co-authored-by: JZY_SC30<jiangzhiyou@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3662 merge feature/qwen3 into master
    
    [pytorch][feature]update ckpt_convert script to v2 (Qwen3&Qwen3_moe)
    
    Created-by: JZY_SC30
    Commit-by: JZY_SC30
    Merged-by: ascend-robot
    Description: update ckpt_convert script to v2 (qwen3&qwen3moe)
    
    See merge request: Ascend/MindSpeed-LLM!3662

--- examples/mcore/qwen3/ckpt_convert_qwen3_mcore2hf.sh
+++ examples/mcore/qwen3/ckpt_convert_qwen3_mcore2hf.sh
@@ -2,15 +2,9 @@
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-python convert_ckpt.py \
-    --use-mcore-models \
-    --model-type GPT \
+python convert_ckpt_v2.py \
     --load-model-type mg \
     --save-model-type hf \
-    --target-tensor-parallel-size 1 \
-    --target-pipeline-parallel-size 1 \
-    --spec mindspeed_llm.tasks.models.spec.qwen3_spec layer_spec \
     --load-dir ./model_weights/qwen3_mcore/ \
-    --save-dir ./model_from_hf/qwen3_hf/ \
-    --params-dtype bf16 \
+    --save-dir ./hf_weights/qwen3_hf/ \
     --model-type-hf qwen3
