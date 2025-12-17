354a6388c23b52558df96c41dee01e2c97416922
jzh6229 <jiangzhihui4@huawei.com>
2025-12-17 09:23:35 +0800

    [pytorch][bugfix] update ling-mini not support mtp
    Co-authored-by: jzh6229<jiangzhihui4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3948 merge 2.2.0 into 2.2.0
    
    [pytorch][bugfix] update ling-mini not support mtp
    
    Created-by: jzh6229
    Commit-by: jzh6229
    Merged-by: ascend-robot
    Description: [pytorch][bugfix] update ling-mini not support mtp
    
    See merge request: Ascend/MindSpeed-LLM!3948

--- mindspeed_llm/features_manager/transformer/mtp.py
+++ mindspeed_llm/features_manager/transformer/mtp.py
@@ -48,3 +48,9 @@ class MultiTokenPredictionFeature(MindSpeedFeature):
                                       mtp_block_build_layers_wrapper)
         patch_manager.register_patch('megatron.core.transformer.multi_token_prediction.MultiTokenPredictionBlock.forward',
                                       mtp_block_forward)
+
+    def validate_args(self, args):
+        LING_MINI_HIDDEN_SIZE = 2048
+        if hasattr(args, 'spec') and args.spec == ["mindspeed_llm.tasks.models.spec.bailing_spec","layer_spec"]:
+            if args.mtp_num_layers is not None and args.mtp_num_layers >= 1 and args.hidden_size == LING_MINI_HIDDEN_SIZE:
+                raise AssertionError('ling-mini-2.0 is not support mtp_num_layers.')
