f4067a23de38a1e84e25b7a9893aed81d99730d7
ascend-robot <zhongyuanke@huawei.com>
2026-01-20 21:48:51 +0800

    [pytorch][feat]:adaptor te gmm tensor paramter initialization
    Co-authored-by: lizhi<lizhi166@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4056 merge master into master
    
    [pytorch][feat]:adaptor te gmm tensor paramter initialization
    
    Created-by: liz_hw
    Commit-by: lizhi
    Merged-by: ascend-robot
    Description: feat:adaptor te gmm tensor paramter initialization
    同步mindspeed-core适配 ： https://gitcode.com/Ascend/MindSpeed/pull/3172
    
    See merge request: Ascend/MindSpeed-LLM!4056

--- mindspeed_llm/features_manager/megatron_basic/transformer_engine_basic.py
+++ mindspeed_llm/features_manager/megatron_basic/transformer_engine_basic.py
@@ -23,6 +23,13 @@ class TransformerEngineBasicFeature(MindSpeedFeature):
                            default=False, help='Compare the cast and quantmatmul of te on cpu and npu online.')
         group.add_argument('--te-comparison-with-bf16', action='store_true',
                            default=False, help='Compare the cast and quantmatmul of te with bf16 online.')
+        group.add_argument('--te-gmm-mode',
+                           choices=['performance', 'compatible'],
+                           default='compatible',
+                           help='Select the TE-GMM execution mode. '
+                                    '"performance": Enables high-performance optimizations. '
+                                    '"compatible": Default. Ensures compatibility with native TE behavior.',
+                           dest='te_gmm_mode')
 
     def validate_args(self, args):
         if args.fp8 and args.transformer_impl == 'local':
@@ -47,13 +54,28 @@ class TransformerEngineBasicFeature(MindSpeedFeature):
         pm.register_patch('transformer_engine.pytorch.tensor.QuantizedTensor', torch.nn.Module, create_dummy=True)
 
     def register_patches(self, pm: MindSpeedPatchesManager, args):
+        if not getattr(args, 'te_gmm_mode', 'compatible') == 'performance':
+            from mindspeed.te.pytorch.module.grouped_linear import MindSpeedTEGroupedLinear, \
+                MindSpeedTEColumnParallelGroupedLinear, MindSpeedTERowParallelGroupedLinear
+            pm.register_patch('megatron.core.extensions.transformer_engine.TEGroupedLinear', MindSpeedTEGroupedLinear)
+            pm.register_patch('megatron.core.extensions.transformer_engine.TEColumnParallelGroupedLinear',
+                            MindSpeedTEColumnParallelGroupedLinear)
+            pm.register_patch('megatron.core.extensions.transformer_engine.TERowParallelGroupedLinear',
+                            MindSpeedTERowParallelGroupedLinear)
+        else:
+            from mindspeed.te.pytorch.module.performance_grouped_linear import MindSpeedTEPerformanceGroupedLinear, \
+                MindSpeedTEPerformanceColumnParallelGroupedLinear, MindSpeedTEPerformanceRowParallelGroupedLinear
+            pm.register_patch('megatron.core.extensions.transformer_engine.TEGroupedLinear',
+                                MindSpeedTEPerformanceGroupedLinear)
+            pm.register_patch('megatron.core.extensions.transformer_engine.TEColumnParallelGroupedLinear',
+                                MindSpeedTEPerformanceColumnParallelGroupedLinear)
+            pm.register_patch('megatron.core.extensions.transformer_engine.TERowParallelGroupedLinear',
+                                MindSpeedTEPerformanceRowParallelGroupedLinear)
         if getattr(args, "fp8_format", False):
             from mindspeed.te.pytorch.attention.dot_product_attention.dot_product_attention import \
                 MindSpeedTEDotProductAttention
             from mindspeed.te.pytorch.module.layernorm_column_parallel_linear import \
                 MindSpeedTELayerNormColumnParallelLinear
-            from mindspeed.te.pytorch.module.grouped_linear import MindSpeedTEGroupedLinear, \
-                MindSpeedTEColumnParallelGroupedLinear, MindSpeedTERowParallelGroupedLinear
             from mindspeed.te.pytorch.module.linear import TERowParallelLinear, TEColumnParallelLinear
             from mindspeed.te.pytorch.fp8.constants import Format, Fp8Recipe
             from mindspeed.core.fp8_utils import get_fp8_context
@@ -70,11 +92,6 @@ class TransformerEngineBasicFeature(MindSpeedFeature):
 
             pm.register_patch('megatron.core.extensions.transformer_engine.TELayerNormColumnParallelLinear',
                               MindSpeedTELayerNormColumnParallelLinear)
-            pm.register_patch('megatron.core.extensions.transformer_engine.TEGroupedLinear', MindSpeedTEGroupedLinear)
-            pm.register_patch('megatron.core.extensions.transformer_engine.TEColumnParallelGroupedLinear',
-                              MindSpeedTEColumnParallelGroupedLinear)
-            pm.register_patch('megatron.core.extensions.transformer_engine.TERowParallelGroupedLinear',
-                              MindSpeedTERowParallelGroupedLinear)
 
             pm.register_patch('transformer_engine.common.recipe.Format', Format)
             pm.register_patch('megatron.core.enums.Fp8Recipe', Fp8Recipe)
@@ -112,8 +129,6 @@ class TransformerEngineBasicFeature(MindSpeedFeature):
             from megatron.core.tensor_parallel.layers import ColumnParallelLinear, RowParallelLinear
             from mindspeed.te.pytorch.module.layernorm_column_parallel_linear import \
                 MindSpeedTELayerNormColumnParallelLinear
-            from mindspeed.te.pytorch.module.grouped_linear import MindSpeedTEGroupedLinear, \
-                MindSpeedTEColumnParallelGroupedLinear, MindSpeedTERowParallelGroupedLinear
 
             if not getattr(args, 'use_ascend_mc2', False):
                 pm.register_patch('megatron.core.extensions.transformer_engine.TEColumnParallelLinear',
@@ -133,8 +148,3 @@ class TransformerEngineBasicFeature(MindSpeedFeature):
 
             pm.register_patch('megatron.core.extensions.transformer_engine.TELayerNormColumnParallelLinear',
                               MindSpeedTELayerNormColumnParallelLinear)
-            pm.register_patch('megatron.core.extensions.transformer_engine.TEGroupedLinear', MindSpeedTEGroupedLinear)
-            pm.register_patch('megatron.core.extensions.transformer_engine.TEColumnParallelGroupedLinear',
-                              MindSpeedTEColumnParallelGroupedLinear)
-            pm.register_patch('megatron.core.extensions.transformer_engine.TERowParallelGroupedLinear',
-                              MindSpeedTERowParallelGroupedLinear)
