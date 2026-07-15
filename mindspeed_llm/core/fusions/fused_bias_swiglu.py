1ed31e0e36aefdd2e868ec8e865164fd0b030480
iansheng <shengjiayi@huawei.com>
2026-07-15 22:00:47 +0800

    fix: fused_swiglu_with_limit remove auto cast
    Co-authored-by: iansheng<shengjiayi@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4764 merge master into master
    
    fix: fused_swiglu_with_limit remove auto cast
    
    Created-by: iansheng
    Commit-by: iansheng
    Merged-by: ascend-robot
    Description: 
    
    ## What this PR does / why we need it?
    
    fix: fused_swiglu_with_limit remove auto cast
    
    The function fused_swiglu_with_limit auto cast input x to float, which cause extra memery consumption.
    
    ## Does this PR introduce any user-facing change?
    
    N/A
    
    ## How was this patch tested?
    
    The training function is operating normally.
    
    See merge request: Ascend/MindSpeed-LLM!4764

--- mindspeed_llm/core/fusions/fused_bias_swiglu.py
+++ mindspeed_llm/core/fusions/fused_bias_swiglu.py
@@ -7,6 +7,7 @@ _SWIGLU_LIMIT = None
 
 def get_swiglu_limit():
     from megatron.training.global_vars import get_args
+
     args = get_args()
     return getattr(args, 'swiglu_limit', 0)
 
@@ -19,7 +20,7 @@ def fused_swiglu_with_limit(x, limit=0):
     else:
         limit = _SWIGLU_LIMIT
     if limit > 0:
-        y_1, y_2 = torch.chunk(x.float(), 2, dim=0)
+        y_1, y_2 = torch.chunk(x, 2, dim=0)
         y_1 = y_1.clamp(min=None, max=limit)
         y_2 = y_2.clamp(min=-limit, max=limit)
         return torch_npu.npu_swiglu(torch.cat([y_1, y_2], dim=0).to(x.dtype), dim=-1)
