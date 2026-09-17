0ebf60e35590f981bf86de42c453e283366e68c2
zhyebin01 <zhangyebin@h-partners.com>
2026-09-17 19:24:57 +0800

    feat(pytorch): update ci envs
    Co-authored-by: zhyebin01<zhangyebin@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5063 merge master into master
    
    feat(pytorch): update ci envs
    
    Created-by: zhyebin01
    Commit-by: zhyebin01
    Merged-by: ascend-robot
    Description: 
    
    ## What this PR does / why we need it?
    
    Update the supporting component versions to fix issues caused by older versions.
    
    ## Does this PR introduce any user-facing change?
    
    NA.
    
    ## How was this patch tested?
    
    pipeline test pass
    
    See merge request: Ascend/MindSpeed-LLM!5063

--- ci/build_ci_envs.sh
+++ ci/build_ci_envs.sh
@@ -21,7 +21,7 @@ fi
 # ============================================================
 BASE_IMAGE="swr.cn-south-1.myhuaweicloud.com/ascendhub/cann:9.1.0-910b-openeuler24.03-py3.12"
 TORCH_VERSION="2.10.0"
-TORCH_NPU_VERSION="2.10.0.post4"
+TORCH_NPU_VERSION="2.10.0.post6"
 TRITON_ASCEND_VERSION="3.2.2"
 MINDSPEED_BRANCH="master"
 MEGATRON_BRANCH="core_v0.12.1"
